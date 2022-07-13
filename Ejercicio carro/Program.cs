9183910796bdda9e28f46185b653b30cdb36fea0
Kevin Arce <kevin_19.6@hotmail.com>
2022-07-12 21:52:43 -0600

    Primer ejercicio

---
+++ Ejercicio carro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ejercicio_carro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehiculo vehiculo1 = new Vehiculo("Geely","Rojo","KVN-197",1500);
+            Console.WriteLine(vehiculo1);
+
+            vehiculo1.informacion();
+            vehiculo1.arrancar();
+            Console.WriteLine("La velocidad es de : " + vehiculo1.LimiteDeVelocidad());
+            Console.WriteLine("La cantidad de pasajeros es de : " + vehiculo1.Pasajeros());
+            vehiculo1.apagar();
+        }
+    }
+}
---
+++ Ejercicio carro/Vehiculo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_carro
+{
+    class Vehiculo
+    {
+        //atributos
+        public int Motor { get; set; }
+        public string Placa { get; set; }
+        public string Color { get; set; }
+
+        public string Marca { get; set; }
+
+
+        //contructor
+
+        public Vehiculo(string marca,string color,string placa, int motor)
+        {
+            Marca = marca;
+            Motor = motor;
+            Color = color;
+            Placa = placa;
+        }
+
+        //metodo
+
+        public void arrancar()
+        {
+            Console.WriteLine("El vehiculo esta prendido");
+        }
+
+        public void apagar()
+        {
+            Console.WriteLine("El vehiculo esta apagado");
+        }
+
+        public void informacion()
+        {
+            Console.WriteLine("El vehiculo es marca : "+ Marca + ", es color: "+ Color + ", su placa es: "+ Placa + " y tiene una cilindrada de: "+ Motor);
+        }
+
+        public int LimiteDeVelocidad()
+        {
+            Random rnd = new Random();
+            int velocidad = rnd.Next(1, 240);
+            return velocidad;
+          
+        }
+        public string Pasajeros()
+        {
+            Random rnd = new Random();
+            int intPasajeros = rnd.Next(1, 4);
+            string strpasajeros = intPasajeros.ToString();
+            return strpasajeros; 
+        }
+
+
+    }
+}
